54f69ae809155cc6b8420ce303c648acf11f560a
Nic Collins <collins.nicolas@gmail.com>
2014-12-08 13:07:54 -0500

    Update install.sh
    Add code for install functions.

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ check_homebrew()
 
 install_homebrew()
 {
-
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
 }
 
 check_node()
@@ -19,7 +20,9 @@ check_node()
 
 install_nvm()
 {
-
+  curl https://raw.githubusercontent.com/NicCollins/nvm/v0.21.0/install.sh | bash
+  source ~/.nvm/nvm.sh
+  nvm install stable
 }
 
 check_android()
@@ -29,5 +32,20 @@ check_android()
 
 install_android()
 {
+  brew install android-sdk
+}
 
+setup_ios()
+{
+ sudo authorize_ios 
+}
+
+check_appium()
+{
+  
+}
+
+install_appium()
+{
+  npm -g install appium
 }
